6309683660d3d9b32c04b964667ffb6215471659
Yakamoz-dev <syz8734181@126.com>
2021-08-31 18:18:20 +0800

    contact form add new empty input verify.

--- app/code/Ecomteck/AdvancedContact/Controller/Form/Post.php
+++ app/code/Ecomteck/AdvancedContact/Controller/Form/Post.php
@@ -36,52 +36,63 @@ class Post extends Action
      */
     public function execute()
     {
-        $validator = $this->_objectManager->get(\Magento\Framework\Data\Form\FormKey\Validator::class);
-        if ($validator->validate($this->getRequest())) {
-            $helper = $this->_objectManager->get(\Ecomteck\AdvancedContact\Helper\Data::class);
-            $json = $this->_objectManager->get(\Magento\Framework\Serialize\Serializer\Json::class);
-            $store = $this->_objectManager->get(\Magento\Store\Model\StoreManagerInterface::class)->getStore();
-            $fields = $helper->getConfig('advanced_contact/fields', $store->getId());
-            $message = $helper->getConfig('advanced_contact/thanks_message', $store->getId());
-            $fields = $json->unserialize($fields);
-            $info = [];
-            if (count($fields)>0) {
-                foreach ($fields as $field) {
-                    if ($this->getRequest()->getParam($field['key'])) {
-                        $info[] = [
-                            'key' => $field['key'],
-                            'label' => $field['label'],
-                            'type' => $field['field_type'],
-                            'value' => $this->getRequest()->getParam($field['key'])
-                        ];
-                    }
-                }
-            }
-            if (count($info)>0) {
-                $model = $this->_objectManager->get(\Ecomteck\AdvancedContact\Model\Request::class);
-                $model->setData('info', $json->serialize($info));
-                try {
-                    $model->save();
-                    if ($model->getId()) {
-                        $email = $this->_objectManager->get(\Ecomteck\AdvancedContact\Helper\Email::class);
-                        $email->receive($model, $store->getId());
-                        if(!$message){
-                            $message = __('Thanks for contacting us with your comments and questions. We\'ll respond to you very soon.');
+        if ($this->getRequest()->getParam("lastname") != "") {
+            $messageManager = $this->_objectManager
+                ->get(\Magento\Framework\Message\ManagerInterface::class);
+            $this->messageManager->addError(
+                __('Sorry, We can\'t save contact messasge or send emails. Please try again later.')
+            );
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+            return $resultRedirect;
+        } else {
+            $validator = $this->_objectManager->get(\Magento\Framework\Data\Form\FormKey\Validator::class);
+            if ($validator->validate($this->getRequest())) {
+                $helper = $this->_objectManager->get(\Ecomteck\AdvancedContact\Helper\Data::class);
+                $json = $this->_objectManager->get(\Magento\Framework\Serialize\Serializer\Json::class);
+                $store = $this->_objectManager->get(\Magento\Store\Model\StoreManagerInterface::class)->getStore();
+                $fields = $helper->getConfig('advanced_contact/fields', $store->getId());
+                $message = $helper->getConfig('advanced_contact/thanks_message', $store->getId());
+                $fields = $json->unserialize($fields);
+                $info = [];
+                if (count($fields)>0) {
+                    foreach ($fields as $field) {
+                        if ($this->getRequest()->getParam($field['key'])) {
+                            $info[] = [
+                                'key' => $field['key'],
+                                'label' => $field['label'],
+                                'type' => $field['field_type'],
+                                'value' => $this->getRequest()->getParam($field['key'])
+                            ];
                         }
-                        $messageManager = $this->_objectManager
-                            ->get(\Magento\Framework\Message\ManagerInterface::class);
-                        $messageManager->addSuccess($message);
                     }
-                } catch (\Exception $e) {
-                    $this->messageManager->addError(
-                        __('Sorry, We can\'t save contact messasge or send emails. Please try again later.')
-                    );
-                    $this->messageManager->addError($e->getMessage());
+                }
+                if (count($info)>0) {
+                    $model = $this->_objectManager->get(\Ecomteck\AdvancedContact\Model\Request::class);
+                    $model->setData('info', $json->serialize($info));
+                    try {
+                        $model->save();
+                        if ($model->getId()) {
+                            $email = $this->_objectManager->get(\Ecomteck\AdvancedContact\Helper\Email::class);
+                            $email->receive($model, $store->getId());
+                            if(!$message){
+                                $message = __('Thanks for contacting us with your comments and questions. We\'ll respond to you very soon.');
+                            }
+                            $messageManager = $this->_objectManager
+                                ->get(\Magento\Framework\Message\ManagerInterface::class);
+                            $messageManager->addSuccess($message);
+                        }
+                    } catch (\Exception $e) {
+                        $this->messageManager->addError(
+                            __('Sorry, We can\'t save contact messasge or send emails. Please try again later.')
+                        );
+                        $this->messageManager->addError($e->getMessage());
+                    }
                 }
             }
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+            return $resultRedirect;
         }
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setUrl($this->_redirect->getRefererUrl());
-        return $resultRedirect;
     }
 }

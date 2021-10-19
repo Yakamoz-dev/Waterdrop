5b46b914d761c6520a811bf499308825a2f327c8
Yakamoz-dev <syz8734181@126.com>
2021-10-19 15:05:57 +0800

    change mobile product collection breadcrumbs styles && add new module coupon info.

---
+++ app/code/Ecopure/Catalog/Block/CouponInfo.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ecopure\Catalog\Block;
+
+class CouponInfo extends \Magento\Framework\View\Element\Template
+{
+    protected $_coreRegistry = null;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_coreRegistry = $registry;
+        $this->_storeManager = $context->getStoreManager();
+        parent::__construct($context,$data);
+    }
+
+    public function getProduct()
+    {
+        if ($product = $this->_coreRegistry->registry('current_product')) {
+            return $product;
+        }else{
+            return null;
+        }
+    }
+}

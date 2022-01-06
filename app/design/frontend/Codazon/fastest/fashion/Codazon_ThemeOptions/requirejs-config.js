6c581b103461413046f4e21ade6dc8b091c661a3
Lxk12121 <lxk12121@163.com>
2022-01-06 17:04:03 +0800

    Magento2A+模板

---
+++ app/design/frontend/Codazon/fastest/fashion/Codazon_ThemeOptions/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        'swipeslider':"Codazon_ThemeOptions/js/swiper-bundle.min"
+    },
+    shim: {
+        'swipeslider': {
+            deps:['jquery']
+        }
+    }
+};
